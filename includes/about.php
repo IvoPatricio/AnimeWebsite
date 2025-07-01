87569e59ce0bbc19cd9c1037ae8c1a5dbc2b7abd
IvoPatricio <snoukefm@gmail.com>
2025-07-01 20:46:10 +0100

    Update files: upcoming, contact, about

--- includes/about.php
+++ includes/about.php
@@ -1,8 +1,10 @@
 <section id="onepage_about" class="container-fluid p-0 d-flex flex-column">
   <div class="row justify-content-center">
     <div class="col-auto">
-      <h2 class="text-center mb-4">Upcoming Animes</h2>
-      <p>Here are the upcoming animes I'm excited about...</p>
+      <h2 class="title-text">About</h2>
     </div>
   </div>
+
+  <div class="row row-minheight d-flex align-items-stretch justify-content-start">
+    <div class="col-12 p-0 d-flex"></div>
 </section>
--- includes/upcoming.php
+++ includes/upcoming.php
@@ -1,8 +1,33 @@
 <section id="onepage_upcoming_animes" class="container-fluid p-0 d-flex flex-column">
   <div class="row justify-content-center">
     <div class="col-auto">
-      <h2 class="text-center mb-4">Upcoming Animes</h2>
-      <p>Here are the upcoming animes I'm excited about...</p>
+      <h2 class="title-text">Upcoming Animes</h2>
     </div>
+  </div>
+
+  <div class="row row-minheight d-flex align-items-stretch justify-content-start">
+    <div class="col-2 p-0 d-flex"></div>
+    <div class="col-4 p-0 d-flex">
+      <img
+        src="images/favoriteBleach.jpg"
+        class="img-fluid logo_img_main" />
+    </div>
+    <div class="col-4 m-2 d-flex">
+      <p class="regular-text">Text text</p>
+    </div>
+    <div class="col-2 p-0 d-flex"></div>
+  </div>
+
+  <div class="row d-flex row-minheight align-items-stretch justify-content-start">
+    <div class="col-2 p-0 d-flex"></div>
+    <div class="col-4 p-0 d-flex">
+      <img
+        src="images/favoriteRezero.webp"
+        class="img-fluid logo_img_main" />
+    </div>
+    <div class="col-4 m-2 d-flex">
+      <p class="regular-text">Text text</p>
+    </div>
+    <div class="col-2 p-0 d-flex"></div>
   </div>
 </section>
