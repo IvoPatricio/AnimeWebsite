2aeffeb2bdb4479735ed215511256b3e395d9630
IvoPatricio <snoukefm@gmail.com>
2025-07-01 21:50:52 +0100

    Create .gitignore and database config: prevents database password leaks

---
+++ includes/config.php
@@ -0,0 +1,19 @@
+<?php
+
+$passwords = require 'dbpasswords.php';
+
+$servername = $passwords['servername'];
+$database = $passwords['database']; 
+$username = $passwords['username'];
+$password = $passwords['password'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else {
+    echo "Connected successfully";
+}
