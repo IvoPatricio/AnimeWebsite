a1720a75f054be81323aa3d3cf372c8419f30a85
IvoPatricio <snoukefm@gmail.com>
2025-07-01 21:12:02 +0100

    Update footer: text and css

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,10 @@
 <section id="onepage_footer" class="container-fluid p-0 d-flex flex-column">
-  <div class="row justify-content-center">
-    <div class="col-auto">
-      <h2 class="title-text">Footer</h2>
+    <div class="row justify-content-start">
+        <div class="col">
+            <p class="footer-text d-flex align-items-center m-0">
+                <i class="fas fa-copyright"></i>
+                2025 Ivo Marques - Creative Work. Feel free to steal
+            </p>
+        </div>
     </div>
-  </div>
 </section>
