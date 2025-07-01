0d93e94bec772110b1b0687f77d7a55423ea3a4c
IvoPatricio <snoukefm@gmail.com>
2025-07-01 23:31:29 +0100

    Update favorites: fetch data from database

--- includes/config.php
+++ includes/config.php
@@ -14,6 +14,3 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-else {
-    echo "Connected successfully";
-}
--- includes/favorites.php
+++ includes/favorites.php
@@ -1,3 +1,9 @@
+<?php
+  include 'config.php';
+  $sql = "SELECT title, image_path, description FROM favorites";
+  $result = $conn->query($sql);
+?>
+
 <section id="onepage_favorites" class="container-fluid p-0 d-flex flex-column">
   <div class="row justify-content-center">
     <div class="col-auto">
@@ -5,29 +11,34 @@
     </div>
   </div>
 
-  <div class="row row-minheight d-flex align-items-stretch justify-content-start">
-    <div class="col-2 p-0 d-flex"></div>
-    <div class="col-4 p-0 d-flex">
-      <img
-        src="images/favoriteBleach.jpg"
-        class="img-fluid logo_img_main" />
-    </div>
-    <div class="col-4 m-2 d-flex">
-      <p class="regular-text">Text text</p>
-    </div>
-    <div class="col-2 p-0 d-flex"></div>
-  </div>
+  <?php
+  // Assuming $conn is your MySQLi connection object
 
-  <div class="row d-flex row-minheight align-items-stretch justify-content-start">
-    <div class="col-2 p-0 d-flex"></div>
-    <div class="col-4 p-0 d-flex">
-      <img
-        src="images/favoriteRezero.webp"
-        class="img-fluid logo_img_main" />
-    </div>
-    <div class="col-4 m-2 d-flex">
-      <p class="regular-text">Text text</p>
-    </div>
-    <div class="col-2 p-0 d-flex"></div>
-  </div>
+  $sql = "SELECT title, image_path, description FROM favorites";
+  $result = $conn->query($sql);
+
+  if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "
+        <div class='row row-minheight d-flex align-items-stretch justify-content-start'>
+          <div class='col-2 p-0 d-flex'></div>
+
+          <div class='col-4 p-0 d-flex'>
+            <img 
+              src='images/". htmlspecialchars($row['image_path']) . "'
+              class='img-fluid logo_img_main' alt='" . htmlspecialchars($row['title']) . "' />
+          </div>
+
+          <div class='col-4 m-2 d-flex'>
+            <p class='regular-text'>" . htmlspecialchars($row['description']) . "</p>
+          </div>
+
+          <div class='col-2 p-0 d-flex'></div>
+        </div>
+        ";
+    }
+  } else {
+    echo '<p>No favorite animes found.</p>';
+  }
+  ?>
 </section>
